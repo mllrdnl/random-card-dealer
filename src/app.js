959751069431994ad22da356e993cf5f211cff47
mllrdnl <mllr.dnl@gmail.com>
2021-07-20 04:18:38 +0000

    set getCard function

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  const getCard = list => {
+    let randomNumber = Math.floor(Math.random() * list.length);
+
+    return list[randomNumber];
+  };
+
+  document.querySelector(".card-center").innerHTML = getCard(cardList);
 };
